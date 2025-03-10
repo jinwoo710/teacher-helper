c9060f91ce9275565d6d66123b5abc63d546f67a
jinwoo710 <jinwoo710@naver.com>
2025-03-10 18:16:31 +0900

    학생 등록 페이지 작업중

--- src/routes/layouts/Default.tsx
+++ src/routes/layouts/Default.tsx
@@ -3,7 +3,7 @@ import { Outlet } from 'react-router-dom'
 
 export default function RootLayout() {
   return (
-    <div className="min-h-screen w-full bg-red-200">
+    <div className="min-h-screen w-full bg-red-200 text-gray-800">
       <main className="mx-auto max-w-[1024px] w-full px-4 bg-orange-200">
         <Header />
         <Outlet />
---
+++ src/routes/pages/students/components/StudentNameTag.tsx
@@ -0,0 +1,14 @@
+import { Student } from "../types";
+import boyIcon from '@/assets/images/icons/boy.png'
+import girlIcon from '@/assets/images/icons/girl.png'
+
+interface StudentNameTag {
+    student: Student
+}
+export default function StudentNameTag({student}:StudentNameTag) {
+return (
+    <div className={`flex w-full rounded-lg p-4 space-x-2 border cursor-pointer h-fit ${student.gender == '남' ? 'bg-blue-100' : 'bg-pink-100'}`}>
+        <img src={student.gender == '남' ? boyIcon : girlIcon} alt={student.gender == '남' ? "남자" : "여자"} className="w-6 h-6"/><span>{student.id} . {student.name}</span> 
+    </div>
+)
+}
